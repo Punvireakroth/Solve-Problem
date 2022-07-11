ca025f0590829d8deff04638c4df374b99cd91c3
Punvireakroth <vireakrothpun@gmail.com>
2022-07-11 17:32:15 +0700

    bubble sort

--- problem.py
+++ problem.py
@@ -1,10 +1,29 @@
 # print the amount of prime number
 amount = 0
-for num in range(2, 10):
-    if all(num%i !=0 for i in range(2, num)):
+# for num in range(2, 1000):
+#     if all(num%i !=0 for i in range(2, num)):
+#         amount += 1
+# print(amount)
+
+# for num in range(2, 100):
+#     for i in range(2, num):
+#         if(num%i != 0):
+#             break
+#     else:
+#         print(num)
+#         amount += 1
+# print(amount)
+prime = True
+for num in range(100, 201):
+    for i in range(2, num):
+        if(num%i == 0):
+            prime = False
+            break
+
+    if(prime):
+        print(num)
         amount += 1
+    prime = True
 print(amount)
-
-
 
  
